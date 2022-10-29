226c55f9577464286c8836d65274f223d64a013f
dleh1541 <dleh1541@naver.com>
2022-10-29 11:16:01 +0900

    API 내용 추가

--- src/components/Sample.jsx
+++ src/components/Sample.jsx
@@ -25,8 +25,257 @@ export default function API() {
       <TitleTab>
         <Title>API</Title>
       </TitleTab>
-      <ContentBox>API 내용 적어</ContentBox>
-      <Table>여기 안에 api 내용 적어 테이블로 노션처럼</Table>
+      <ContentBox></ContentBox>
+      <Table>
+        {" "}
+        <table border="1">
+          <th>대분류</th>
+          <th>소분류</th>
+          <th>URI</th>
+          <th>Method</th>
+          <th>설명</th>
+          <tr>
+            <td>FRONT</td>
+            <td>Course</td>
+            <td>/front/course/{"{courseId}"}/rating</td>
+            <td>POST</td>
+            <td>강좌의 평점을 등록합니다.</td>
+          </tr>
+          <tr>
+            <td>FRONT</td>
+            <td>Unit</td>
+            <td>/front/course/{"{courseId}"}/unit</td>
+            <td>GET</td>
+            <td>강의 목록을 조회합니다.</td>
+          </tr>
+          <tr>
+            <td>FRONT</td>
+            <td>Unit</td>
+            <td>
+              /front/course/{"{courseId}"}/unit/{"{unitId}"}
+            </td>
+            <td>GET</td>
+            <td>unitId에 해당하는 강의로 이동합니다.</td>
+          </tr>
+          <tr>
+            <td>FRONT</td>
+            <td>Unit</td>
+            <td>
+              /front/course/{"{courseId}"}/unit/{"{unitId}"}/play
+            </td>
+            <td>POST</td>
+            <td>강의 영상을 재생합니다.</td>
+          </tr>
+          <tr>
+            <td>FRONT</td>
+            <td>Question</td>
+            <td>
+              /front/course/{"{courseId}"}/unit/{"{unitId}"}/question
+            </td>
+            <td>POST</td>
+            <td>유닛의 질문을 등록합니다.</td>
+          </tr>
+          <tr>
+            <td>FRONT</td>
+            <td>Question</td>
+            <td>
+              /front/course/{"{courseId}"}/unit/{"{unitId}"}/question
+            </td>
+            <td>GET</td>
+            <td>유닛의 질문 목록을 조회합니다.</td>
+          </tr>
+          <tr>
+            <td>FRONT</td>
+            <td>Question</td>
+            <td>
+              /front/course/{"{courseId}"}/unit/{"{unitId}"}/question/
+              {"{questionId}"}
+            </td>
+            <td>PATCH</td>
+            <td>유닛의 질문을 수정합니다.</td>
+          </tr>
+          <tr>
+            <td>FRONT</td>
+            <td>Question</td>
+            <td>
+              /front/course/{"{courseId}"}/unit/{"{unitId}"}/question/
+              {"{questionId}"}
+            </td>
+            <td>DELETE</td>
+            <td>유닛의 질문을 삭제합니다.</td>
+          </tr>
+          <tr>
+            <td>FRONT</td>
+            <td>Reply</td>
+            <td>
+              /front/course/{"{courseId}"}/unit/{"{unitId}"}/question/reply/
+              {"{replyId}"}
+            </td>
+            <td>GET</td>
+            <td>질문에 대한 답변 목록을 조회합니다.</td>
+          </tr>
+          <tr>
+            <td>FRONT</td>
+            <td>Reply</td>
+            <td>
+              /front/course/{"{courseId}"}/unit/{"{unitId}"}/question/reply/
+              {"{replyId}"}
+            </td>
+            <td>PATCH</td>
+            <td>질문에 대한 답변을 수정합니다.</td>
+          </tr>
+          <tr>
+            <td>FRONT</td>
+            <td>Reply</td>
+            <td>
+              /front/course/{"{courseId}"}/unit/{"{unitId}"}/question/reply/
+              {"{replyId}"}/{"{replyId}"}
+            </td>
+            <td>DELETE</td>
+            <td>질문에 대한 답변을 삭제합니다.</td>
+          </tr>
+          <tr>
+            <td>FRONT</td>
+            <td>Reply</td>
+            <td>
+              /front/course/{"{courseId}"}/unit/{"{unitId}"}/question/reply/
+              {"{replyId}"}/reply
+            </td>
+            <td>POST</td>
+            <td>질문에 대한 답변을 등록합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Authentication</td>
+            <td>/open/auth/login</td>
+            <td>POST</td>
+            <td>로그인합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Authentication</td>
+            <td>/open/auth/logout</td>
+            <td>POST</td>
+            <td>로그아웃합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Authentication</td>
+            <td>/open/auth/reissue</td>
+            <td>POST</td>
+            <td>토큰을 재발급합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Authentication</td>
+            <td>/open/auth/signup</td>
+            <td>POST</td>
+            <td>회원가입합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Course</td>
+            <td>/open/course</td>
+            <td>POST</td>
+            <td>강좌를 개설합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Course</td>
+            <td>/open/course/cancel</td>
+            <td>POST</td>
+            <td>courseId에 해당하는 강좌를 수강취소합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Course</td>
+            <td>/open/course/register</td>
+            <td>POST</td>
+            <td>courseId에 해당하는 강좌를 수강신청합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Course</td>
+            <td>/open/course/{"{courseId}"}</td>
+            <td>GET</td>
+            <td>courseId에 해당하는 강좌 정보를 조회합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Course</td>
+            <td>/open/course/{"{courseId}"}</td>
+            <td>PATCH</td>
+            <td>courseId에 해당하는 강좌 정보를 수정합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Course</td>
+            <td>/open/course/{"{courseId}"}</td>
+            <td>DELETE</td>
+            <td>courseId에 해당하는 강좌를 삭제합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Course</td>
+            <td>/open/course/{"{courseId}"}/advance</td>
+            <td>GET</td>
+            <td>courseId에 해당하는 강좌의 나의 진척도를 조회합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Course</td>
+            <td>/open/course/{"{courseId}"}/rating</td>
+            <td>GET</td>
+            <td>courseId에 해당하는 강좌의 평점을 조회합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Player</td>
+            <td>/open/player/off</td>
+            <td>POST</td>
+            <td>player를 종료합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Player</td>
+            <td>/open/player/on</td>
+            <td>POST</td>
+            <td>player를 재생합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Unit</td>
+            <td>/open/course/{"{courseId}"}/unit</td>
+            <td>POST</td>
+            <td>강의를 등록합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Unit</td>
+            <td>/open/course/{"{courseId}"}/unit</td>
+            <td>Get</td>
+            <td>unitId에 해당하는 강의 정보를 조회합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Unit</td>
+            <td>
+              /open/course/{"{courseId}"}/unit/{"{unitId}"}
+            </td>
+            <td>PATCH</td>
+            <td>unitId에 해당하는 강의를 수정합니다.</td>
+          </tr>
+          <tr>
+            <td>OPEN</td>
+            <td>Unit</td>
+            <td>
+              /open/course/{"{courseId}"}/unit/{"{unitId}"}
+            </td>
+            <td>DELETE</td>
+            <td>unitId에 해당하는 강의를 삭제합니다.</td>
+          </tr>
+        </table>
+      </Table>
     </Wrapper>
   );
 }
--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -21,17 +21,23 @@ export default function Tutorial() {
       <TitleTab>
         <Title>Tutorial</Title>
       </TitleTab>
-      <ContentBox>우리 플레이어 소개</ContentBox>
-      <div>그냥 내용 적을거</div>
+      <ContentBox>
+        Sim Player는 사용자 편의성과 보안성이 강화된 웹 미디어 플레이어입니다.
+      </ContentBox>
+      <div>다양한 플랫폼에서 사용 가능하도록 API를 제공합니다.</div>
       <br />
       <br />
       <br />
-      <div> 만약 개조식으로 쓴다면 이렇게</div>
+      <div>Sim Player는 다음의 기능을 지원합니다.</div>
       <ul>
-        <li>머시기</li>
-        <li>머시기</li>
-        <li>머시기</li>
-        <li>머시기</li>
+        <li>배속 기능</li>
+        <li>전체 화면</li>
+        <li>강의 목차 이동</li>
+        <li>PIP 모드</li>
+        <li>자막 기능</li>
+        <li>시간대별 질문 등록 및 확인 기능</li>
+        <li>중복 접속 제어</li>
+        <li>강의 평점 기능</li>
       </ul>
     </Wrapper>
   );
