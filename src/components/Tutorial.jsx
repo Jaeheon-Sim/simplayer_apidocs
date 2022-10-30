788520a74c71cd4ea4b6201e189a9c43ca0fcd53
dleh1541 <dleh1541@naver.com>
2022-10-30 15:01:16 +0900

    튜토리얼 내용 추가

--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -28,17 +28,59 @@ export default function Tutorial() {
       <br />
       <br />
       <br />
-      <div>Sim Player는 다음의 기능을 지원합니다.</div>
+      <div>Sim Player는 다음의 기본적인 플레이어의 기능을 지원합니다.</div>
       <ul>
         <li>배속 기능</li>
         <li>전체 화면</li>
         <li>강의 목차 이동</li>
         <li>PIP 모드</li>
         <li>자막 기능</li>
-        <li>시간대별 질문 등록 및 확인 기능</li>
-        <li>중복 접속 제어</li>
-        <li>강의 평점 기능</li>
       </ul>
+      <div>
+        또한 기존 플레이어의 단점을 개선한 다음의 주요 기능들을 제공합니다.
+      </div>
+
+      <h2>시간대별 질문/답변 기능</h2>
+      <img
+        src="img/feature1_question.png"
+        width="70%"
+        height="70%"
+        alt="feature1"
+      />
+      <div>
+        강의를 듣던 중 질문 사항이 생겼을 때, 사이드 바를 통해 바로 질문을 남길
+        수 있습니다.
+        <br />
+        기존의 분리되어 있던 온라인 강의 공간과 질문 공간을 일원화함으로써
+        사용자 편의성을 개선하였습니다.
+      </div>
+
+      <h2>중복 로그인 제어</h2>
+      <img
+        src="img/feature2_loginControl.png"
+        width="70%"
+        height="70%"
+        alt="feature2"
+      />
+      <div>
+        동일한 아이디로 중복 접속 시 접속 및 수강을 제어합니다.
+        <br />
+        이를 통해 부정 수강을 방지하고 향상된 보안성을 제공합니다.
+      </div>
+
+      <h2>강의 평가 기능</h2>
+      <img
+        src="img/feature3_evaluation.png"
+        width="70%"
+        height="70%"
+        alt="feature3"
+      />
+      <div>
+        일정 시간 이상 강의를 시청하면 강의 평가를 남길 수 있습니다.
+        <br />
+        학습자는 등록된 강의 평점을 통해 자신이 원하는 강의를 선별할 수
+        있습니다.
+      </div>
     </Wrapper>
   );
 }
